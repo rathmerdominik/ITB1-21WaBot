ecdb7a2f961be3bb88437fc1aaea95e5ceff45a6
rathmerdominik <rathmerdominik@outlook.de>
2021-08-28 18:04:24 +0200

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ITB1-21WaBot
 Whatsapp Bot for the managment of the ITB1-21 Group
+
+## Installation
+To install this bot you need to do the following: 
+```
+npm install github:adiwajshing/baileys chokidar
+git clone https://github.com/rathmerdominik/ITB1-21WaBot.git
+```
+
+## Setting up the Environment Variables
+You need to setup the following Environment variables 
+```
+WABOT_CREDSPATH=    //The folder where your Whatsapp credentials should be saved and read
+WABOT_ADMINNUMBER=  //The number that is allowed to execute special commands from the bot  
+WABOT_FILEROOT=     //The root folder of your Filebrowser instance
+WABOT_GROUPTOSEND=  //The group where updates should be send
+WABOT_ATTACHURL=    //The Url that should be attached to instantly link the new file
+WABOT_SPLITBYUSER=  //The User from which the Changes should be read on 
+```
+
+## Setting up the Whatsapp Environment
+
+It is required that you have a phone with a seperate whatsapp instance that can stay online for the entire time while the bot is running.
+
+Launch the Bot
+```
+node index.js
+```
+Scan the QR-Code appearing on your screen and put your phone in a location where it can stay and charge until you decide to shutdown the bot
---
+++ index.js
@@ -0,0 +1,124 @@
+/**
+ * For now this Bot is just intended to read out the Root directory for Filebrowser and then Send it over to the Whatsapp Group.
+ */
+
+import { MessageType, Mimetype, WAConnection } from '@adiwajshing/baileys'
+import * as fs from 'fs'
+import * as chokidar from 'chokidar'
+import { exit } from 'process'
+
+if(!process.env.WABOT_CREDSPATH) throw Error("Set the WABOT_CREDSPATH environment variable!") 
+if(!process.env.WABOT_ADMINNUMBER) throw Error("Set the WABOT_ADMINNUMBER environment variable!")
+if(!process.env.WABOT_FILEROOT) throw Error("Set the WABOT_FILEROOT environment variable!")
+if(!process.env.WABOT_GROUPTOSEND) throw Error("Set the WABOT_GROUPTOSEND environment variable!")
+if(!process.env.WABOT_ATTACHURL) throw Error("Set the WABOT_ATTACHURL environment variable!")
+if(!process.env.WABOT_SPLITBYUSER) throw Error("Set the WABOT_SPLITBYUSER environment variable!")
+
+const splitByUser = process.env.WABOT_SPLITBYUSER
+const groupToSend = process.env.WABOT_GROUPTOSEND
+const credsPath = process.env.WABOT_CREDSPATH
+const adminNumber = process.env.WABOT_ADMINNUMBER
+const fileRoot = process.env.WABOT_FILEROOT
+const attachURL = process.env.WABOT_ATTACHURL
+
+const conn = new WAConnection() 
+//set usable adminNumber
+const realAdmin = adminNumber + "@s.whatsapp.net"
+
+//check if credentials exist
+fs.stat(credsPath + "/creds.json", (err) => { //check for Credentials
+    if(err){
+        console.log(err)
+        return
+    } else {
+       
+        //Load the current credentials
+        conn.loadAuthInfo (credsPath + '/creds.json')
+        return
+    }
+})
+
+conn.on('open', () => {
+
+            const authInfo = conn.base64EncodedAuthInfo()
+            fs.writeFileSync(credsPath + '/creds.json', JSON.stringify(authInfo, null, '\t'))
+            var watcher = chokidar.watch(fileRoot, {ignored: /^\./, persistent: true, ignoreInitial: true});
+            watcher
+            .on('ready', function(path) {
+                conn.sendMessage(
+                    groupToSend, 
+                    "Monitoring for new Files!",
+                    MessageType.text)
+            })
+            .on('add', function(path) {
+                const basePath = path.split(splitByUser)
+                const genURL = attachURL + basePath[1]
+                const realURL = genURL.replace(' ', "%20")
+                
+                conn.sendMessage(
+                    groupToSend, 
+                    "Neue Datei wurde hochgeladen! \n" + realURL,
+                    MessageType.text)
+            })
+            .on('error', function(error) {console.error('Error happened', error);})
+})
+
+conn.on('chat-update', async chat => { 
+
+    if (!chat.hasNewMessage) {
+        return
+    } 
+
+    const m = chat.messages.all()[0]
+
+    if(m.key.fromMe){
+        return
+    }
+    MessageType, MessageOptions, Mimetype
+    if(!m.message) return
+
+    const conv = m.message.conversation
+    const chatNumber = m.key.remoteJid
+
+    if(chat.messages && chat.count){
+        switch(conv){
+            case "$suicide":
+                if(m.participant !== realAdmin) return
+                await conn.sendMessage(m.key.remoteJid, "Killing myself, Goodbye!", MessageType.text)
+                conn.close()
+                break;
+        }
+    }
+})
+
+//be the gigachad
+conn.on('group-participants-update', async group => {
+    
+    const mygroup = group.jid
+    if(group.action === "add"){
+        if(mygroup !== groupToSend){
+            await conn.sendMessage(
+                group.jid, 
+                fs.readFileSync("./media/gigachad.jpg"),
+                MessageType.image,
+                    {
+                        mimetype: Mimetype.jpeg, 
+                        caption: "> gets added to group\n> tells everyone to fuck off \n> refuses to elaborate \n> leaves"
+                    }
+                )
+            await conn.groupLeave(group.jid)
+        }
+    }
+
+})
+
+conn.on('close', ({reason, isReconnecting}) => {
+    if(reason === "intentional"){
+        console.log("Successfully killed myself! Goodbye")
+        exit(0)
+    } else {
+        console.log("Oh shit. I died cuz of "+ reason +"! Do i reconnect? " + isReconnecting)
+    }
+})
+await conn.connect() 
+
